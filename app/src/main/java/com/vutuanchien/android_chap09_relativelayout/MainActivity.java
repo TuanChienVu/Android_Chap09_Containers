a735818226ff1cd8d865edcc4c35904a9afc0380
VuTuanChien <jbtuanchien.11@gmail.com>
2016-03-11 12:02:37 +0700

    Android_Chap09_RelativeLayout
    Android_Chap09_RelativeLayout

---
+++ app/src/main/java/com/vutuanchien/android_chap09_relativelayout/MainActivity.java
@@ -0,0 +1,41 @@
+package com.vutuanchien.android_chap09_relativelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnCancel;
+    EditText edName;
+    EditText edPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edName = (EditText) findViewById(R.id.edName);
+        edPass = (EditText) findViewById(R.id.edPass);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Your Name: " + edName.getText() + "\nYour Pass: " + edPass.getText(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
